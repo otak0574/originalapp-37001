082dd5611fb2eb0f0c423391a38e836971a50ed8
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-27 00:48:35 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,13 @@ Rails.application.routes.draw do
   devise_scope :store do
     get 'store_addresses', to: 'stores/registrations#new_store_address'
     post 'store_addresses', to: 'stores/registrations#create_store_address'
+    get 'store_addresses/:id/edit', to: 'stores/registrations#edit_store_address'
+    patch 'store_addresses/:id', to: 'stores/registrations#update_store_address'
 
     get 'store_informations', to: 'stores/registrations#new_store_information'
     post 'store_informations', to: 'stores/registrations#create_store_information'
+    get 'store_informations/:id/edit', to: 'stores/registrations#edit_store_information'
+    patch 'store_informations/:id', to: 'stores/registrations#update_store_information'
   end
   devise_for :customers, controllers: {
     registrations: "customers/registrations",
@@ -22,5 +26,6 @@ Rails.application.routes.draw do
 }
 
 resources :publicstores, only: [:show, :edit, :update]
+resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
