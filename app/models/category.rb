de56b25062abfbfca2f40d680d8e40904b6dab16
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-01 08:59:38 +0900

    デプロイ環境への適応

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,13 @@ class Category < ApplicationRecord
   has_many :store_categories
   has_many :stores, through: :store_categories
 
+  def self.setup_initial_categories
+    categories = [
+      '食料品', '調味料', 'お酒', '備蓄食料', '日用品', 'ギフト', 'お菓子', 'デザート', '化粧品', 'お子様', 'ペット', 'その他'
+    ]
+
+    categories.each do |name|
+      Category.find_or_create_by(name: name)
+    end
+  end
 end
