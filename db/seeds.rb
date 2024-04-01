7d42dc8a7197f0e6b39048b534d7ca06d19a0910
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-01 09:28:36 +0900

    re:デプロイ環境への適応

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = [
+  '食料品', '調味料', 'お酒',  '備蓄食料', '日用品', 'ギフト', 'お菓子', 'デザート', '化粧品', 'お子様', 'ペット', 'その他'
+]
+Category.destroy_all
+categories.each do |name|
+  Category.create(name: name)
+end
