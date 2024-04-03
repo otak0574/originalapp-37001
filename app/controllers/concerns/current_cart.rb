dd0857a7a5b2ed74fde4cdf2cf49ea8345ea41e5
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-04 07:45:04 +0900

    カート機能の編集

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,9 +1,13 @@
 module CurrentCart
 	private
 	def set_cart
+        item = Item.find(params[:item_id])
 	    @cart = Cart.find(session[:cart_id])
         rescue ActiveRecord::RecordNotFound
-        @cart = Cart.create(customer_id: current_customer.id)
+            binding.pry
+
+        @cart = Cart.create(customer_id: current_customer.id, store_id: item.store_id)
         session[:cart_id] = @cart.id
+        end
     end
 end
