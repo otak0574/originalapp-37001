6f17fb1e6039428ccf64e94a133d44c6a84f5e5f
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-05 08:29:32 +0900

    トークンのアソシエーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
   attr_accessor :token
+  belongs_to :user
+  belongs_to :store
+  validates :user_id, presence: true
   validates :price, presence: true
-
+  validates :token, presence: true
 end
