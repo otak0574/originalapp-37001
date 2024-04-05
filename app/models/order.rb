412a327702ef1ff7eb0b9b09f76e3ff004e339b5
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-05 09:55:17 +0900

    アソシエーションの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   attr_accessor :token
-  belongs_to :user
+  belongs_to :customer
   belongs_to :store
-  validates :user_id, presence: true
+  validates :customer_id, presence: true
   validates :price, presence: true
   validates :token, presence: true
 end
