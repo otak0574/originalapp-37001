7d59caf149b114415bdbfd3213ab78fab78fd7ae
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-06 11:56:31 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,6 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :pref_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :customer_id
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
