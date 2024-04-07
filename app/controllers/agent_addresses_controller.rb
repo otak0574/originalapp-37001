70d080da6f151d5be978792b806c60b6e8d58030
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-08 07:44:43 +0900

    アドレス情報入力画面

--- app/controllers/agent_addresses_controller.rb
+++ app/controllers/agent_addresses_controller.rb
@@ -1,2 +1,36 @@
 class AgentAddressesController < ApplicationController
+
+  def new
+    @agent_address = AgentAddress.new
+  end
+
+  def create
+    @agent_address = AgentAddress.new(agent_address_params)
+    if @agent_address.save
+      redirect_to @agent_address, notice: 'Agent address was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def agent_address_params
+    params.require(:agent_address).permit(:postal_code, :pref_id, :city, :house_number, :building_name).merge(deli_agent_id: current_deli_agent.id)
+  end
 end
