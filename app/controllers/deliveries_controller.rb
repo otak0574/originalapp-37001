f907b5d4671ffe1d79a8a0f4b05277df634830e9
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-04-10 13:01:43 +0900

    店舗情報の取得

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,6 +1,12 @@
 class DeliveriesController < ApplicationController
 
   def index
-    @full_agent_address = current_deli_agent.agent_address
+    @store_address = current_deli_agent.agent_address
+    @order = Order.all
+  end
+
+  def show 
+    order = Order.find(params[:id])
+    @cart = order.cart
   end
 end
